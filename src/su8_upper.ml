e69892ebc65c1cc42a556d1cc7c41309940715ac
Florent Monnier <monnier.florent@gmail.com>
2026-05-05 22:39:21 +0200

    add'd some u8 chars

--- src/su8_upper.ml
+++ src/su8_upper.ml
@@ -44,13 +44,32 @@ let u8 s =
   let cs = rev_map to_int cs in
   fld_lft (fun (prev, acc) c ->
     match prev, c with
-    | Some 0xc3, 0xb6 -> (None, 0x96::acc) (*o*)
+    | Some 0xc3, 0xb6 -> (None, 0x96::acc) (*o*) (* uml's *)
     | Some 0xc3, 0xa4 -> (None, 0x84::acc) (*a*)
     | Some 0xc3, 0xab -> (None, 0x8b::acc) (*e*)
     | Some 0xc3, 0xbc -> (None, 0x9c::acc) (*u*)
     | Some 0xc3, 0xaf -> (None, 0x8f::acc) (*i*)
     | Some 0xc3, 0xbf -> (None, 0xb8::0xc5::(pop 1 acc)) (*y*)
+    | Some 0xc3, 0xa9 -> (None, 0x89::acc)  (* é *)  (* "éèêà" *)
+    | Some 0xc3, 0xa8 -> (None, 0x88::acc)  (* è *)
+    | Some 0xc3, 0xaa -> (None, 0x8a::acc)  (* ê *)
+    | Some 0xc3, 0xa0 -> (None, 0x80::acc)  (* à *)
+
+    | Some 0xc3, 0xa2 -> (None, 0x82::acc)  (* â *)
+    | Some 0xc3, 0xb4 -> (None, 0x94::acc)  (* ô *)
+    | Some 0xc3, 0xae -> (None, 0x8e::acc)  (* î *)
+    | Some 0xc3, 0xbb -> (None, 0x9b::acc)  (* û *)
+    | Some 0xc3, 0xb9 -> (None, 0x99::acc)  (* ù *)
+
+    | Some 0xc3, 0xa7 -> (None, 0x87::acc)  (* ç *)
+
     | _, b -> (Some c, b::acc)
+    (*
+    *)
+    (*
+      c3 82 c3 94 c3 8e c3 9b c3 99
+      Â  Ô  Î  Û  Ù
+    *)
   ) (None, []) cs
 
 let of_int = char_of_int ;;
